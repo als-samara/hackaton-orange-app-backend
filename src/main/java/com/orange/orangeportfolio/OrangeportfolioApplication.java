8f9e4559326e83522008d3bb7db09ee68b144086
als-samara <samaraalmeida379@gmail.com>
2024-01-22 23:12:20 -0300

    first commit

---
+++ src/main/java/com/orange/orangeportfolio/OrangeportfolioApplication.java
@@ -0,0 +1,13 @@
+package com.orange.orangeportfolio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrangeportfolioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrangeportfolioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/orange/orangeportfolio/OrangeportfolioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.orange.orangeportfolio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrangeportfolioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
