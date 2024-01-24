efc3551c9a3d080e425434f9004af21750f806b6
Larissa Chiaradia <lari.chiaradia.silva@gmail.com>
2024-01-23 21:36:04 -0300

    create

---
+++ src/main/java/com/orange/orangeportfolio/model/User.java
@@ -0,0 +1,56 @@
+package com.orange.orangeportfolio.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+
+@Entity
+@Table(name = "tb_user")
+public class User {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	@NotBlank(message = "Name cannot be blank")
+	private String nome;
+	@NotBlank(message = "Sobrenome cannot be blank")
+	private String sobrenome;
+	@Email
+	private String email;
+	private String senha;
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getSobrenome() {
+		return sobrenome;
+	}
+	public void setSobrenome(String sobrenome) {
+		this.sobrenome = sobrenome;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getSenha() {
+		return senha;
+	}
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+}
