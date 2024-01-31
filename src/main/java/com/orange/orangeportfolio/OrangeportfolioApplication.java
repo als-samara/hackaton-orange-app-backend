0d4b815f3a022f7e9ef1aedafae641f0d581f312
als-samara <samaraalmeida379@gmail.com>
2024-01-31 19:00:21 -0300

    ImageControllerTest

--- src/main/java/com/orange/orangeportfolio/OrangeportfolioApplication.java
+++ src/main/java/com/orange/orangeportfolio/OrangeportfolioApplication.java
@@ -2,10 +2,8 @@ package com.orange.orangeportfolio;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
-@EnableFeignClients
 public class OrangeportfolioApplication {
 
 	public static void main(String[] args) {
