30727cdd32f29f1b412de2bb061e32784f098a85
Larissa Chiaradia <lari.chiaradia.silva@gmail.com>
2024-02-04 23:12:18 -0300

    feat: Created method to search images by user_id

--- src/main/java/com/orange/orangeportfolio/repository/ImageRepository.java
+++ src/main/java/com/orange/orangeportfolio/repository/ImageRepository.java
@@ -1,5 +1,6 @@
 package com.orange.orangeportfolio.repository;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -10,4 +11,6 @@ import com.orange.orangeportfolio.model.Image;
 @Repository
 public interface ImageRepository extends JpaRepository<Image, Long>{
 	Optional<Image> findByName(String name);
+	
+	public List<Image> findAllByUserId(Long userId);
 }
